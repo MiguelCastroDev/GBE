5b9185450d03b256546effd30f281164041e2915
Miguel Angel <miguel@miguelcastro.net>
2021-04-13 13:16:08 +0200

    [ADD] - Project and structure for desktop app

---
+++ GBE/Desktop GBE/Form1.Designer.cs
@@ -0,0 +1,306 @@
+﻿namespace Desktop_GBE
+{
+    partial class menuPanel
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(menuPanel));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.devNameLabel = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.configButton = new System.Windows.Forms.Button();
+            this.flowLayoutPanel4 = new System.Windows.Forms.FlowLayoutPanel();
+            this.servicesButton = new System.Windows.Forms.Button();
+            this.flowLayoutPanel2 = new System.Windows.Forms.FlowLayoutPanel();
+            this.clientsButton = new System.Windows.Forms.Button();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.quitButton = new System.Windows.Forms.Button();
+            this.dashboardButton = new System.Windows.Forms.Button();
+            this.logoNavBarPanel = new System.Windows.Forms.Panel();
+            this.softwareNameLabel = new System.Windows.Forms.Label();
+            this.flowLayoutPanel3 = new System.Windows.Forms.FlowLayoutPanel();
+            this.workersButton = new System.Windows.Forms.Button();
+            this.infoButton = new System.Windows.Forms.Button();
+            this.viewPanel = new System.Windows.Forms.Panel();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.logoNavBarPanel.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.LightSeaGreen;
+            this.panel1.Controls.Add(this.infoButton);
+            this.panel1.Controls.Add(this.devNameLabel);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(933, 47);
+            this.panel1.TabIndex = 0;
+            // 
+            // devNameLabel
+            // 
+            this.devNameLabel.AutoSize = true;
+            this.devNameLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.devNameLabel.ForeColor = System.Drawing.Color.White;
+            this.devNameLabel.Location = new System.Drawing.Point(12, 9);
+            this.devNameLabel.Name = "devNameLabel";
+            this.devNameLabel.Size = new System.Drawing.Size(158, 25);
+            this.devNameLabel.TabIndex = 0;
+            this.devNameLabel.Text = "MikiCastro Dev";
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.LightSeaGreen;
+            this.panel2.Controls.Add(this.configButton);
+            this.panel2.Controls.Add(this.flowLayoutPanel4);
+            this.panel2.Controls.Add(this.servicesButton);
+            this.panel2.Controls.Add(this.flowLayoutPanel2);
+            this.panel2.Controls.Add(this.clientsButton);
+            this.panel2.Controls.Add(this.flowLayoutPanel1);
+            this.panel2.Controls.Add(this.quitButton);
+            this.panel2.Controls.Add(this.dashboardButton);
+            this.panel2.Controls.Add(this.logoNavBarPanel);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel2.Location = new System.Drawing.Point(0, 47);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(230, 525);
+            this.panel2.TabIndex = 1;
+            // 
+            // configButton
+            // 
+            this.configButton.BackColor = System.Drawing.Color.LightSeaGreen;
+            this.configButton.FlatAppearance.BorderSize = 0;
+            this.configButton.FlatAppearance.MouseOverBackColor = System.Drawing.Color.SeaShell;
+            this.configButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.configButton.Image = ((System.Drawing.Image)(resources.GetObject("configButton.Image")));
+            this.configButton.Location = new System.Drawing.Point(180, 475);
+            this.configButton.Name = "configButton";
+            this.configButton.Size = new System.Drawing.Size(50, 50);
+            this.configButton.TabIndex = 7;
+            this.configButton.UseVisualStyleBackColor = false;
+            // 
+            // flowLayoutPanel4
+            // 
+            this.flowLayoutPanel4.BackColor = System.Drawing.Color.SeaShell;
+            this.flowLayoutPanel4.Location = new System.Drawing.Point(0, 266);
+            this.flowLayoutPanel4.Name = "flowLayoutPanel4";
+            this.flowLayoutPanel4.Size = new System.Drawing.Size(10, 50);
+            this.flowLayoutPanel4.TabIndex = 6;
+            // 
+            // servicesButton
+            // 
+            this.servicesButton.BackColor = System.Drawing.Color.LightSeaGreen;
+            this.servicesButton.FlatAppearance.BorderSize = 0;
+            this.servicesButton.FlatAppearance.MouseOverBackColor = System.Drawing.Color.SeaShell;
+            this.servicesButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.servicesButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.servicesButton.Image = ((System.Drawing.Image)(resources.GetObject("servicesButton.Image")));
+            this.servicesButton.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.servicesButton.Location = new System.Drawing.Point(3, 266);
+            this.servicesButton.Name = "servicesButton";
+            this.servicesButton.Size = new System.Drawing.Size(227, 50);
+            this.servicesButton.TabIndex = 5;
+            this.servicesButton.Text = "Servicios";
+            this.servicesButton.UseVisualStyleBackColor = false;
+            this.servicesButton.Click += new System.EventHandler(this.servicesButton_Click);
+            // 
+            // flowLayoutPanel2
+            // 
+            this.flowLayoutPanel2.BackColor = System.Drawing.Color.SeaShell;
+            this.flowLayoutPanel2.Location = new System.Drawing.Point(0, 158);
+            this.flowLayoutPanel2.Name = "flowLayoutPanel2";
+            this.flowLayoutPanel2.Size = new System.Drawing.Size(10, 50);
+            this.flowLayoutPanel2.TabIndex = 4;
+            // 
+            // clientsButton
+            // 
+            this.clientsButton.BackColor = System.Drawing.Color.LightSeaGreen;
+            this.clientsButton.FlatAppearance.BorderSize = 0;
+            this.clientsButton.FlatAppearance.MouseOverBackColor = System.Drawing.Color.SeaShell;
+            this.clientsButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.clientsButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.clientsButton.Image = ((System.Drawing.Image)(resources.GetObject("clientsButton.Image")));
+            this.clientsButton.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.clientsButton.Location = new System.Drawing.Point(3, 158);
+            this.clientsButton.Name = "clientsButton";
+            this.clientsButton.Size = new System.Drawing.Size(227, 50);
+            this.clientsButton.TabIndex = 3;
+            this.clientsButton.Text = "Clientes";
+            this.clientsButton.UseVisualStyleBackColor = false;
+            this.clientsButton.Click += new System.EventHandler(this.clientsButton_Click);
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.BackColor = System.Drawing.Color.SeaShell;
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(0, 106);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(10, 50);
+            this.flowLayoutPanel1.TabIndex = 2;
+            // 
+            // quitButton
+            // 
+            this.quitButton.BackColor = System.Drawing.Color.LightSeaGreen;
+            this.quitButton.FlatAppearance.BorderSize = 0;
+            this.quitButton.FlatAppearance.MouseOverBackColor = System.Drawing.Color.SeaShell;
+            this.quitButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.quitButton.Image = ((System.Drawing.Image)(resources.GetObject("quitButton.Image")));
+            this.quitButton.Location = new System.Drawing.Point(0, 475);
+            this.quitButton.Name = "quitButton";
+            this.quitButton.Size = new System.Drawing.Size(50, 50);
+            this.quitButton.TabIndex = 2;
+            this.quitButton.UseVisualStyleBackColor = false;
+            this.quitButton.Click += new System.EventHandler(this.quitButton_Click);
+            // 
+            // dashboardButton
+            // 
+            this.dashboardButton.BackColor = System.Drawing.Color.LightSeaGreen;
+            this.dashboardButton.FlatAppearance.BorderSize = 0;
+            this.dashboardButton.FlatAppearance.MouseOverBackColor = System.Drawing.Color.SeaShell;
+            this.dashboardButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.dashboardButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dashboardButton.Image = ((System.Drawing.Image)(resources.GetObject("dashboardButton.Image")));
+            this.dashboardButton.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.dashboardButton.Location = new System.Drawing.Point(3, 106);
+            this.dashboardButton.Name = "dashboardButton";
+            this.dashboardButton.Size = new System.Drawing.Size(227, 50);
+            this.dashboardButton.TabIndex = 1;
+            this.dashboardButton.Text = "Escritorio";
+            this.dashboardButton.UseVisualStyleBackColor = false;
+            this.dashboardButton.Click += new System.EventHandler(this.dashboardButton_Click);
+            // 
+            // logoNavBarPanel
+            // 
+            this.logoNavBarPanel.Controls.Add(this.softwareNameLabel);
+            this.logoNavBarPanel.Dock = System.Windows.Forms.DockStyle.Top;
+            this.logoNavBarPanel.Location = new System.Drawing.Point(0, 0);
+            this.logoNavBarPanel.Name = "logoNavBarPanel";
+            this.logoNavBarPanel.Size = new System.Drawing.Size(230, 100);
+            this.logoNavBarPanel.TabIndex = 0;
+            // 
+            // softwareNameLabel
+            // 
+            this.softwareNameLabel.AutoSize = true;
+            this.softwareNameLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.softwareNameLabel.ForeColor = System.Drawing.Color.White;
+            this.softwareNameLabel.Location = new System.Drawing.Point(64, 41);
+            this.softwareNameLabel.Name = "softwareNameLabel";
+            this.softwareNameLabel.Size = new System.Drawing.Size(122, 24);
+            this.softwareNameLabel.TabIndex = 0;
+            this.softwareNameLabel.Text = "GBE Desktop";
+            // 
+            // flowLayoutPanel3
+            // 
+            this.flowLayoutPanel3.BackColor = System.Drawing.Color.SeaShell;
+            this.flowLayoutPanel3.Location = new System.Drawing.Point(0, 259);
+            this.flowLayoutPanel3.Name = "flowLayoutPanel3";
+            this.flowLayoutPanel3.Size = new System.Drawing.Size(10, 50);
+            this.flowLayoutPanel3.TabIndex = 4;
+            // 
+            // workersButton
+            // 
+            this.workersButton.BackColor = System.Drawing.Color.LightSeaGreen;
+            this.workersButton.FlatAppearance.BorderSize = 0;
+            this.workersButton.FlatAppearance.MouseOverBackColor = System.Drawing.Color.SeaShell;
+            this.workersButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.workersButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.workersButton.Image = ((System.Drawing.Image)(resources.GetObject("workersButton.Image")));
+            this.workersButton.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.workersButton.Location = new System.Drawing.Point(3, 259);
+            this.workersButton.Name = "workersButton";
+            this.workersButton.Size = new System.Drawing.Size(227, 50);
+            this.workersButton.TabIndex = 3;
+            this.workersButton.Text = "Trabajadores";
+            this.workersButton.UseVisualStyleBackColor = false;
+            this.workersButton.Click += new System.EventHandler(this.workersButton_Click);
+            // 
+            // infoButton
+            // 
+            this.infoButton.BackColor = System.Drawing.Color.LightSeaGreen;
+            this.infoButton.FlatAppearance.BorderSize = 0;
+            this.infoButton.FlatAppearance.MouseOverBackColor = System.Drawing.Color.SeaShell;
+            this.infoButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.infoButton.Image = ((System.Drawing.Image)(resources.GetObject("infoButton.Image")));
+            this.infoButton.Location = new System.Drawing.Point(885, 0);
+            this.infoButton.Name = "infoButton";
+            this.infoButton.Size = new System.Drawing.Size(48, 47);
+            this.infoButton.TabIndex = 8;
+            this.infoButton.UseVisualStyleBackColor = false;
+            // 
+            // viewPanel
+            // 
+            this.viewPanel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.viewPanel.Location = new System.Drawing.Point(230, 47);
+            this.viewPanel.Name = "viewPanel";
+            this.viewPanel.Size = new System.Drawing.Size(703, 525);
+            this.viewPanel.TabIndex = 5;
+            // 
+            // menuPanel
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(933, 572);
+            this.Controls.Add(this.viewPanel);
+            this.Controls.Add(this.flowLayoutPanel3);
+            this.Controls.Add(this.workersButton);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "menuPanel";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.logoNavBarPanel.ResumeLayout(false);
+            this.logoNavBarPanel.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label devNameLabel;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Button dashboardButton;
+        private System.Windows.Forms.Panel logoNavBarPanel;
+        private System.Windows.Forms.Label softwareNameLabel;
+        private System.Windows.Forms.Button quitButton;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel2;
+        private System.Windows.Forms.Button clientsButton;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel3;
+        private System.Windows.Forms.Button workersButton;
+        private System.Windows.Forms.Button configButton;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel4;
+        private System.Windows.Forms.Button servicesButton;
+        private System.Windows.Forms.Button infoButton;
+        private System.Windows.Forms.Panel viewPanel;
+    }
+}
+
---
+++ GBE/Desktop GBE/Form1.cs
@@ -0,0 +1,65 @@
+﻿using Desktop_GBE.Forms;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Desktop_GBE
+{
+    public partial class menuPanel : Form
+    {
+        public menuPanel()
+        {
+            InitializeComponent();
+            openDashboard();
+        }
+
+        private void quitButton_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void openForm(object form)
+        {
+            if(this.viewPanel.Controls.Count > 0){
+                this.viewPanel.Controls.RemoveAt(0);
+            }
+            Form view = form as Form;
+            view.TopLevel = false;
+            view.Dock = DockStyle.Fill;
+            this.viewPanel.Controls.Add(view);
+            this.viewPanel.Tag = view;
+            view.Show();
+        }
+
+        private void dashboardButton_Click(object sender, EventArgs e)
+        {
+            openDashboard();
+        }
+
+        private void openDashboard()
+        {
+            openForm(new Dashboard());
+        }
+
+        private void clientsButton_Click(object sender, EventArgs e)
+        {
+            openForm(new ClientsForm());
+        }
+
+        private void workersButton_Click(object sender, EventArgs e)
+        {
+            openForm(new WorkersForm());
+        }
+
+        private void servicesButton_Click(object sender, EventArgs e)
+        {
+            openForm(new ServicesForm());
+        }
+    }
+}
